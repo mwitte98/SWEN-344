07714fda96ccea30e8a82a5e97263a512b93e843
Ryan Hoffmann <rhoff37@gmail.com>
2015-10-02 18:05:48 -0400

    Updated server modules

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 //  Server Stuff
 
 var express = require('express');
-var exphbs = require('express-handlebars');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(methodOverride()); // simulate DELETE and PUT
+
 // Set to the port to run the server on
 var port = process.env.PORT || 8080;
-
-// Get all data from POST parameters
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Listen on 'port' -> localhost:8080
 app.listen(port);
