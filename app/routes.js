77fcfdf0c5c3982946fa8bace5ced77c9dfda8e1
Ryan Hoffmann <rhoff37@gmail.com>
2015-12-06 11:42:42 -0500

    Hopefully fixed Twitter bugs

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,8 @@ module.exports = function(app, passport, Twit, io) {
     var configAuth = require('../app/config/auth.js');
     var moment = require('moment');
 
+    var TWITTER_EVENT_HANDLERS_SET = false;
+
     app.get('/login', function(req, res) {
         res.render('login');
     });
@@ -38,54 +40,71 @@ module.exports = function(app, passport, Twit, io) {
          access_token_secret: req.user.tokenSecret
       });
 
-      io.sockets.on('connection', function (socket) {
+      if(!TWITTER_EVENT_HANDLERS_SET) { // We haven't yet set up the twitter/socket event handlers (first time to index page)
 
-         socket.on('request tweets', function() { // User is on the home page on the front-end
+         io.sockets.on('connection', function (socket) {
 
-            var TWEET_COUNT = 20;
+            socket.on('request tweets', function() { // User is on the home page on the front-end
 
-            Twitter.get('statuses/home_timeline', {count: TWEET_COUNT}, function(err, tweets, response) {
-               if (err) {
-                  socket.emit('twitter api error');
-               }
-               else {
-                  console.log("Got Home Timeline Tweets");
-                  tweets.forEach(function(tweet, index) {
-                     Twitter.get('statuses/oembed', {id: tweet.id_str}, function(err, oEmbedData, response) {
-                        tweet.oEmbed = oEmbedData;
-                        if( index == TWEET_COUNT - 1 ) { // if this is the last tweet that we got from the API, send a flag for the front-end
-                           socket.emit('new tweet', {tweet: tweet, lastTweet: true});
-                        }
-                        else {
-                           socket.emit('new tweet', {tweet: tweet, lastTweet: false});
-                        }
+               console.log('Request Tweets Event Fired on Server');
 
+               var TWEET_COUNT = 20;
+
+               Twitter.get('statuses/home_timeline', {count: TWEET_COUNT}, function(err, tweets, response) {
+                  if (err) {
+                     socket.emit('twitter api error');
+                  }
+                  else {
+                     console.log("Got Home Timeline Tweets");
+                     tweets.forEach(function(tweet, index) {
+                        Twitter.get('statuses/oembed', {id: tweet.id_str}, function(err, oEmbedData, response) {
+                           tweet.oEmbed = oEmbedData;
+                           if( index == TWEET_COUNT - 1 ) { // if this is the last tweet that we got from the API, send a flag for the front-end
+                              console.log('Last Tweet on Server');
+                              socket.emit('new tweet', {tweet: tweet, lastTweet: true});
+                           }
+                           else {
+                              socket.emit('new tweet', {tweet: tweet, lastTweet: false});
+                           }
+
+                        });
                      });
-                  });
-               }
-            });
-
-         });
-
-         socket.on('request twitter stream', function() { // User is on the home page on the front-end
-
-            // ***** Get Tweets From Twitter *****
-
-            var stream = Twitter.stream('user');
-
-            stream.on('tweet', function(tweet) {
-               console.log("New tweet!");
-               Twitter.get('statuses/oembed', {id: tweet.id_str}, function(err, data, response) {
-                  tweet.oEmbed = data;
-                  socket.emit('new stream tweet', tweet);
+                  }
                });
+
+            });
+
+            socket.on('request twitter stream', function() { // User is on the home page on the front-end
+
+               // ***** Get Tweets From Twitter *****
+
+               console.log('Request Twitter Stream on Server');
+
+               var stream = Twitter.stream('user');
+
+               stream.on('tweet', function(tweet) {
+                  console.log("New tweet!");
+                  Twitter.get('statuses/oembed', {id: tweet.id_str}, function(err, data, response) {
+                     if(err) {
+                        console.log('Stream Error: ' + err);
+                     }
+                     else {
+                        tweet.oEmbed = data;
+                        socket.emit('new stream tweet', tweet);
+                     }
+                  });
+               });
+
             });
 
          });
 
-      });
+         TWITTER_EVENT_HANDLERS_SET = true;
 
-        res.render('index.ejs', {user : req.user, requestTweets: true, events: todaysEvents}); // get the user out of session and pass to template
+      } // end TWITTER_EVENT_HANDLERS_SET if statement
+
+      res.render('index.ejs', {user : req.user, requestTweets: true, events: todaysEvents}); // get the user out of session and pass to template
+
     });
 
     app.get('/logout', isLoggedIn, function(req, res) {
